7f6f17525febaaa5f55a854916401dd4b01ef0d2
Ikenna <uikenna97@gmail.com>
2022-06-21 22:31:11 +0100

    First commit

---
+++ task_1.js
@@ -0,0 +1,10 @@
+var name = "Ikenna Ugwuegbulam"
+var height = "1.80m"
+var country = "Nigeria"
+
+
+
+
+console.log("My name is " + name)
+console.log("My height is " + height)
+console.log("I am from " + country)
